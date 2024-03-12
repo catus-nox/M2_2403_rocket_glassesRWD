d71fd4567ba81f36b6f836916658ae48c01a6f55
cetus <cetnox.c19h21h3o.gmail.com>
2024-03-12 17:45:29 +0800

    js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function load(url, callback) {
  const xhr = new XMLHttpRequest();
  xhr.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
+  // if (this.readyState === 4 && this.status === 200) {
    callback(this.responseText);
-  }
+  // }
  };
  xhr.open('GET', url, true);
  xhr.send();
